80f2aba78e2e435ee7812d9a396032fa14975ff7
jose <joselchecaexpo@gmail.com>
2021-07-13 19:36:41 +0100

    5-add helmet to escuturas and create sculturas page on dato

--- src/pages/esculturas.js
+++ src/pages/esculturas.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { HelmetDatoCms } from 'gatsby-source-datocms'
 import { Flex, useBreakpointValue } from '@chakra-ui/react'
 import { SRLWrapper } from 'simple-react-lightbox'
 
@@ -7,23 +8,26 @@ import { Card } from '../components/Card'
 
 const Esculturas = ({ data }) => {
   const cardWidth = useBreakpointValue({ base: '14em', md: '12em', xl: '14em' })
-  const { allDatoCmsEscultura } = data || {}
+  const { esculturas, pageInfo } = data || {}
   return (
-    <SRLWrapper>
-      <Flex flexWrap="wrap" justifyContent="center">
-        {allDatoCmsEscultura.edges.map(({ node }) => (
-          <Card
-            cursorMode="zoom-in"
-            key={node.id}
-            title={node.title}
-            id={node.id}
-            image={node?.coverImage?.fluid?.src}
-            width={cardWidth}
-            height="16rem"
-          />
-        ))}
-      </Flex>
-    </SRLWrapper>
+    <>
+      <HelmetDatoCms seo={pageInfo.seoMetaTags} />
+      <SRLWrapper>
+        <Flex flexWrap="wrap" justifyContent="center">
+          {esculturas.edges.map(({ node }) => (
+            <Card
+              cursorMode="zoom-in"
+              key={node.id}
+              title={node.title}
+              id={node.id}
+              image={node?.coverImage?.fluid?.src}
+              width={cardWidth}
+              height="16rem"
+            />
+          ))}
+        </Flex>
+      </SRLWrapper>
+    </>
   )
 }
 
@@ -31,7 +35,12 @@ export default Esculturas
 
 export const query = graphql`
   query EscultureQuery {
-    allDatoCmsEscultura {
+    pageInfo: datoCmsEsculturaPage {
+      seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
+      }
+    }
+    esculturas: allDatoCmsEscultura {
       edges {
         node {
           id
