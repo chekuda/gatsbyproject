4d54e934c681078c6b3e937c31e8015d66f99935
jose <joselchecaexpo@gmail.com>
2021-07-30 12:32:13 +0100

    26 add favicon

--- src/templates/MainTemplate/MainTemplate.js
+++ src/templates/MainTemplate/MainTemplate.js
@@ -1,20 +1,31 @@
 import React, { Fragment, useMemo } from 'react'
 import { Flex, Box } from '@chakra-ui/react'
+import { HelmetDatoCms } from 'gatsby-source-datocms'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import { TopNav } from '../../organisms/TopNav'
 
 import './styles.sass'
 
 export const MainTemplate = ({ children, uri }) => {
+  const data = useStaticQuery(graphql`
+    query MainTemplate {
+      main: datoCmsSite {
+        faviconMetaTags {
+          id
+          tags
+        }
+      }
+    }
+  `)
   const isHome = useMemo(() => uri === '/', [uri])
   return (
     <Fragment>
+      <HelmetDatoCms favicon={data?.main?.faviconMetaTags} />
       <Box p="0 20px" m="0 auto" bg="black">
         <TopNav uri={uri} isHome={isHome} />
       </Box>
-      <Flex overflowY="auto">
-        {children}
-      </Flex>
+      <Flex overflowY="auto">{children}</Flex>
     </Fragment>
   )
 }
